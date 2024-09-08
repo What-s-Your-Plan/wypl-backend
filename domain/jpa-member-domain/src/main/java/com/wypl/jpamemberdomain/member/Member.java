ef705e237c8a9f591f071c0894ed75861838678d
jiwonhan <jiwons0803@naver.com>
2024-09-08 20:00:28 +0900

    Feat: Member 도메인 엔티티 작성
    Resolves: #4

--- domain/jpa-member-domain/src/main/java/com/wypl/jpamemberdomain/member/Member.java
+++ domain/jpa-member-domain/src/main/java/com/wypl/jpamemberdomain/member/Member.java
@@ -1,13 +1,38 @@
 package com.wypl.jpamemberdomain.member;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.awt.*;
+import java.time.LocalDate;
 
 @Entity
 public class Member {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(name = "email", length = 50, unique = true, nullable = false)
+	private String email;
+
+	@Column(name = "nickname", length = 20, nullable = false)
+	private String nickname;
+
+	@Column(name = "birthday")
+	private LocalDate birthday;
+
+	@Column(name = "profile_image", length = 100)
+	private String profileImage;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "color", length = 20, nullable = false)
+	private Color color;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "timezone", length = 10, nullable = false)
+	private TimeZone timeZone;
+
+	@OneToMany(mappedBy = "member")
+	private List<MemberCalendar> memberCalendars;
+
 }
