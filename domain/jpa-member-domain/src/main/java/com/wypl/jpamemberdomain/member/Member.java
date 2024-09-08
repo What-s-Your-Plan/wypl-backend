364212dc78d94f0e8ca3c7f5496c4456ac849709
KIMSEI1124 <workju1124@gmail.com>
2024-09-08 18:57:36 +0900

    Feat: Member Domain

---
+++ domain/jpa-member-domain/src/main/java/com/wypl/jpamemberdomain/member/Member.java
@@ -0,0 +1,13 @@
+package com.wypl.jpamemberdomain.member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+}
---
+++ domain/jpa-member-domain/src/test/java/com/wypl/jpamemberdomain/JpaMemberDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wypl.jpamemberdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMemberDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
