9a54021acf7fa53818bc26b66528b8904b03092c
sjhjack <sjhjack@naver.com>
2024-09-15 18:23:09 +0900

    Chore(#21): Repository 어노테이션 추가

--- domain/jpamongo-review-domain/src/main/java/com/wypl/jpamongoreviewdomain/review/repository/ReviewRepository.java
+++ domain/jpamongo-review-domain/src/main/java/com/wypl/jpamongoreviewdomain/review/repository/ReviewRepository.java
@@ -1,8 +1,10 @@
 package com.wypl.jpamongoreviewdomain.review.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.wypl.jpamongoreviewdomain.review.domain.Review;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
--- domain/jpamongo-review-domain/src/main/java/com/wypl/jpamongoreviewdomain/reviewcontents/repository/ReviewContentsRepository.java
+++ domain/jpamongo-review-domain/src/main/java/com/wypl/jpamongoreviewdomain/reviewcontents/repository/ReviewContentsRepository.java
@@ -1,8 +1,10 @@
 package com.wypl.jpamongoreviewdomain.reviewcontents.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.wypl.jpamongoreviewdomain.reviewcontents.domain.ReviewContents;
 
+@Repository
 public interface ReviewContentsRepository extends MongoRepository<ReviewContents, Long> {
 }
