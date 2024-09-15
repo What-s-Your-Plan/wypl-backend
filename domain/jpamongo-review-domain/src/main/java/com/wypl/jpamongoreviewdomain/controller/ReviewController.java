6e2f53e47b9a65b2eeb021d0399148e65bbeebee
sjhjack <sjhjack@naver.com>
2024-09-15 17:11:51 +0900

    Feat(#21): starter-web 추가

---
+++ domain/jpamongo-review-domain/src/main/java/com/wypl/jpamongoreviewdomain/controller/ReviewController.java
@@ -0,0 +1,13 @@
+package com.wypl.jpamongoreviewdomain.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/review")
+public class ReviewController {
+
+}
