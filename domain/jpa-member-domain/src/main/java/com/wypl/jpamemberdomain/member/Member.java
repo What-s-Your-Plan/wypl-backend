9a835d03759f1cc30375683555996ba5399465f2
sjhjack <sjhjack@naver.com>
2024-09-21 15:12:27 +0900

    Refactor(#21): Member Entity에 Getter 추가 및 import 스타일 변경

--- domain/jpa-member-domain/src/main/java/com/wypl/jpamemberdomain/member/Member.java
+++ domain/jpa-member-domain/src/main/java/com/wypl/jpamemberdomain/member/Member.java
@@ -1,10 +1,20 @@
 package com.wypl.jpamemberdomain.member;
 
-import com.wypl.common.Color;
-import jakarta.persistence.*;
-
 import java.time.LocalDate;
 
+import com.wypl.common.Color;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
 @Entity
 @Table(name = "member_tbl")
 public class Member {
