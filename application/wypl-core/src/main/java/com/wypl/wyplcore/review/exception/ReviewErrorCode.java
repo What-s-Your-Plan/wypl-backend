e11edb966168f9b66d1b733e634b2cad3520ccf0
sjhjack <sjhjack@naver.com>
2024-09-21 15:49:32 +0900

    Feat(#21): ReviewException 추가

---
+++ application/wypl-core/src/main/java/com/wypl/wyplcore/review/exception/ReviewErrorCode.java
@@ -0,0 +1,22 @@
+package com.wypl.wyplcore.review.exception;
+
+import com.wypl.common.exception.ServerErrorCode;
+
+import lombok.Getter;
+
+@Getter
+public enum ReviewErrorCode implements ServerErrorCode {
+	EMPTY_CONTENTS(500, "REVIEW_001", "작성된 내용이 없습니다."),
+	INVALID_TITLE(500, "REVIEW_002", "제목의 길이가 올바르지 않습니다.")
+	;
+
+	private final int statusCode;
+	private final String errorCode;
+	private final String message;
+
+	ReviewErrorCode(int statusCode, String errorCode, String message) {
+		this.statusCode = statusCode;
+		this.errorCode = errorCode;
+		this.message = message;
+	}
+}
