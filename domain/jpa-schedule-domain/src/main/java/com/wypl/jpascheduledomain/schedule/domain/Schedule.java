49b2beb1cccc025395b6e80af4489e3fcf2f9f3f
jiwonhan <jiwons0803@naver.com>
2024-09-22 18:05:24 +0900

    Feat: ERD 수정 및 Schedule, ScheduleInfo 엔티티 변경
    Resolves: #14

--- domain/jpa-schedule-domain/src/main/java/com/wypl/jpascheduledomain/schedule/domain/Schedule.java
+++ domain/jpa-schedule-domain/src/main/java/com/wypl/jpascheduledomain/schedule/domain/Schedule.java
@@ -1,13 +1,14 @@
 package com.wypl.jpascheduledomain.schedule.domain;
 
 import com.wypl.jpacommon.JpaBaseEntity;
+import com.wypl.jpascheduledomain.schedule.data.RepetitionCycle;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.SQLRestriction;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -36,4 +37,35 @@ public class Schedule extends JpaBaseEntity {
 
     @Column(name = "end_datetime", nullable = false)
     private LocalDateTime endDateTime;
+
+    @Column(name = "repetition_start_date", nullable = false)
+    private LocalDate repetitionStartDate;
+
+    @Column(name = "repetition_end_date")
+    private LocalDate repetitionEndDate;
+
+    @Enumerated(EnumType.STRING)
+    private RepetitionCycle repetitionCycle; // 반복 주기 (일, 주, 달, 년)
+
+    @Column(name = "day_of_week")
+    private Integer dayOfWeek; // 반복 요일
+
+    @Column(name = "week_interval")
+    private Integer weekInterval; // 주 반복
+
+    // Todo: Review Mapping
+
+    @Builder
+    public Schedule(ScheduleInfo scheduleInfo, String title, String description, LocalDateTime startDateTime, LocalDateTime endDateTime, LocalDate repetitionStartDate, LocalDate repetitionEndDate, RepetitionCycle repetitionCycle, Integer dayOfWeek, Integer weekInterval) {
+        this.scheduleInfo = scheduleInfo;
+        this.title = title;
+        this.description = description;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.repetitionStartDate = repetitionStartDate;
+        this.repetitionEndDate = repetitionEndDate;
+        this.repetitionCycle = repetitionCycle;
+        this.dayOfWeek = dayOfWeek;
+        this.weekInterval = weekInterval;
+    }
 }
--- domain/jpa-schedule-domain/src/main/java/com/wypl/jpascheduledomain/schedule/domain/ScheduleInfo.java
+++ domain/jpa-schedule-domain/src/main/java/com/wypl/jpascheduledomain/schedule/domain/ScheduleInfo.java
@@ -7,7 +7,6 @@ import lombok.*;
 import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -25,33 +24,12 @@ public class ScheduleInfo extends JpaBaseEntity {
     @JoinColumn(name = "calendar_id", nullable = false)
     private Calendar calendar;
 
-    @Column(name = "start_datetime", nullable = false)
-    private LocalDateTime startDateTime;
-
-    @Column(name = "end_datetime", nullable = false)
-    private LocalDateTime endDateTime;
-
     @Column(name = "creator_id")
     private Long creatorId;
-
-    @OneToMany(mappedBy = "scheduleInfo")
-    private List<Repetition> repetitions;
 
     @Builder
     public ScheduleInfo(Calendar calendar, LocalDateTime startDateTime, LocalDateTime endDateTime, Long creatorId) {
         this.calendar = calendar;
-        this.startDateTime = startDateTime;
-        this.endDateTime = endDateTime;
         this.creatorId = creatorId;
-    }
-
-    public void addRepetition(Repetition repetition) {
-        repetitions.add(repetition);
-        repetition.setScheduleInfo(this);
-    }
-
-    public void removeRepetition(Repetition repetition) {
-        repetitions.remove(repetition);
-        repetition.setScheduleInfo(null);
     }
 }
