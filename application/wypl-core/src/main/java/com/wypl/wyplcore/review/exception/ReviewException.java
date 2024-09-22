ec2de3957c69739d00c6a16071fb879bc1dba56c
sjhjack <sjhjack@naver.com>
2024-09-22 16:58:33 +0900

    Refactor(#21): ReviewException의 파라미터를 ServerErrorCode -> ReviewErrorCode로 변경

--- application/wypl-core/src/main/java/com/wypl/wyplcore/review/exception/ReviewException.java
+++ application/wypl-core/src/main/java/com/wypl/wyplcore/review/exception/ReviewException.java
@@ -1,10 +1,9 @@
 package com.wypl.wyplcore.review.exception;
 
-import com.wypl.common.exception.ServerErrorCode;
 import com.wypl.common.exception.WyplException;
 
 public class ReviewException extends WyplException {
-	public ReviewException(ServerErrorCode serverErrorCode) {
+	public ReviewException(ReviewErrorCode serverErrorCode) {
 		super(serverErrorCode);
 	}
 }
