c6ae57761f9ff8b312703f73692710ce86aa3bb8
sjhjack <sjhjack@naver.com>
2024-10-06 19:56:30 +0900

    Feat(#33): Google OAuth 토큰 발급을 위한 객체 추가

---
+++ client/google-oauth-client/src/main/java/com/wypl/googleoauthclient/data/response/GoogleTokenResponse.java
@@ -0,0 +1,21 @@
+package com.wypl.googleoauthclient.data.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
+
+@Builder
+public record GoogleTokenResponse(
+	@JsonProperty("access_token")
+	String accessToken,
+	@JsonProperty("expires_in")
+	int expiresIn,
+	@JsonProperty("refresh_token")
+	String refreshToken,
+	String scope,
+	@JsonProperty("token_type")
+	String tokenType,
+	@JsonProperty("id_token")
+	String idToken
+) {
+}
