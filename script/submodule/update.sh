6b6f42f66b421ac2f4d967751b1368d6bb26f81b
KIMSEI1124 <workju1124@gmail.com>
2024-10-15 12:13:41 +0900

    Feat(#34): git submodule update 스크립트 수정

--- script/submodule/update.sh
+++ script/submodule/update.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-echo "🚀 Start Submodule Update"
+echo "🚀 Navigating to Git root directory..."
+GIT_ROOT=$(git rev-parse --show-toplevel)
 
-git submodule update --recursive
+if [ $? -ne 0 ]; then
+  echo "❌ Error: Not a Git repository."
+  exit 1
+fi
+cd "$GIT_ROOT" || { echo "❌ Failed to change directory to Git root."; exit 1; }
+
+echo "📦 Start Submodule Update"
+
+git submodule update --remote --merge
 git add .
 
 echo '🎯 End Submodule Update'
