282063500a7220bdabc6d51d96aaa475d2804f3d
sjhjack <sjhjack@naver.com>
2024-10-26 11:38:12 +0900

    Chore(#33): 토큰 에러 메시지 추가

--- client/google-oauth-client/src/main/java/com/wypl/googleoauthclient/exception/GoogleOAuthErrorCode.java
+++ client/google-oauth-client/src/main/java/com/wypl/googleoauthclient/exception/GoogleOAuthErrorCode.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 
 @Getter
 public enum GoogleOAuthErrorCode implements ServerErrorCode {
-	BAD_REQUEST(400, "GOOGLE_OAUTH_001", ""),
-	MALFORMED(400, "GOOGLE_OAUTH_002", ""),
+	BAD_REQUEST(400, "GOOGLE_OAUTH_001", "권한이 없습니다."),
+	MALFORMED(400, "GOOGLE_OAUTH_002", "오염된 토큰입니다."),
 	;
 
 	private final int statusCode;
