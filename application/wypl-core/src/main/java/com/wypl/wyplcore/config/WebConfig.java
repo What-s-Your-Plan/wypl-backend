89577ae180e251ca6d4a431bc9aa0fc891ffb0d7
sjhjack <sjhjack@naver.com>
2024-11-03 18:57:04 +0900

    Refactor(#42): AuthenticatedArgumentResolver의 파라미터 추가

--- application/wypl-core/src/main/java/com/wypl/wyplcore/config/WebConfig.java
+++ application/wypl-core/src/main/java/com/wypl/wyplcore/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.wypl.wyplcore.global.config;
+package com.wypl.wyplcore.config;
 
 import java.util.List;
 
@@ -6,13 +6,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.wypl.wyplcore.auth.utils.AuthenticatedArgumentResolver;
+import com.wypl.googleoauthclient.data.AuthMemberService;
+import com.wypl.googleoauthclient.utils.AuthenticatedArgumentResolver;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+	private final AuthMemberService authMemberService;
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-		resolvers.add(new AuthenticatedArgumentResolver());
+		resolvers.add(new AuthenticatedArgumentResolver(authMemberService));
 	}
 }
