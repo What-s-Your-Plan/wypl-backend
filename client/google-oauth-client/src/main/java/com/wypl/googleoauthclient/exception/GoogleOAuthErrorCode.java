c4889d5879b38aa7bb0d06ab985d24d97286aac9
sjhjack <sjhjack@naver.com>
2024-11-03 18:56:21 +0900

    Feat(#42): 토큰 유효성 관련 Exception 추가

--- client/google-oauth-client/src/main/java/com/wypl/googleoauthclient/exception/GoogleOAuthErrorCode.java
+++ client/google-oauth-client/src/main/java/com/wypl/googleoauthclient/exception/GoogleOAuthErrorCode.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 public enum GoogleOAuthErrorCode implements ServerErrorCode {
 	BAD_REQUEST(400, "GOOGLE_OAUTH_001", "권한이 없습니다."),
 	MALFORMED(400, "GOOGLE_OAUTH_002", "올바르지 않은 토큰입니다."),
+	INVALID_TOKEN(400, "GOOGLE_OAUTH_002", "올바르지 않은 값입니다."),
+	NOT_AUTHORIZATION_MEMBER(400, "GOOGLE_OAUTH_002", "인증되지 않은 회원입니다.")
 	;
 
 	private final int statusCode;
