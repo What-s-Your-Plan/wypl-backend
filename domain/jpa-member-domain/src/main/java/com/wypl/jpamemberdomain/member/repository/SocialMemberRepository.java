2bc8a5f92529f00749984053aa66352f41b178f5
sjhjack <sjhjack@naver.com>
2024-11-13 13:58:23 +0900

    Move(#41): SocialMemberRepository 위치 변경

--- domain/jpa-member-domain/src/main/java/com/wypl/jpamemberdomain/member/repository/SocialMemberRepository.java
+++ domain/jpa-member-domain/src/main/java/com/wypl/jpamemberdomain/member/repository/SocialMemberRepository.java
@@ -1,9 +1,10 @@
-package com.wypl.jpamemberdomain.member;
+package com.wypl.jpamemberdomain.member.repository;
 
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.wypl.jpamemberdomain.member.OauthProvider;
 import com.wypl.jpamemberdomain.member.domain.SocialMember;
 
 public interface SocialMemberRepository extends JpaRepository<SocialMember, Long> {
