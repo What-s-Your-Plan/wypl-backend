0f9a6b3cf0962519eb74aaaf4fd81c3f95d47e0a
sjhjack <sjhjack@naver.com>
2024-11-23 17:58:49 +0900

    Feat(#41): 토큰 유효성 검증 시, 토큰 재발급 분기 처리 추기

--- application/wypl-core/src/main/java/com/wypl/wyplcore/auth/service/AuthMemberServiceImpl.java
+++ application/wypl-core/src/main/java/com/wypl/wyplcore/auth/service/AuthMemberServiceImpl.java
@@ -2,9 +2,12 @@ package com.wypl.wyplcore.auth.service;
 
 import org.springframework.stereotype.Component;
 
+import com.wypl.authdomain.auth.service.AuthDomainServiceImpl;
 import com.wypl.googleoauthclient.GoogleOAuthClient;
 import com.wypl.googleoauthclient.data.response.GoogleTokenValidationResponse;
 import com.wypl.googleoauthclient.domain.AuthMember;
+import com.wypl.googleoauthclient.exception.GoogleOAuthErrorCode;
+import com.wypl.googleoauthclient.exception.GoogleOAuthException;
 import com.wypl.googleoauthclient.service.AuthMemberService;
 import com.wypl.jpamemberdomain.member.domain.SocialMember;
 import com.wypl.jpamemberdomain.member.repository.SocialMemberRepository;
@@ -17,15 +20,27 @@ import lombok.RequiredArgsConstructor;
 public class AuthMemberServiceImpl implements AuthMemberService {
 	private final GoogleOAuthClient googleOAuthClient;
 	private final SocialMemberRepository socialMemberRepository;
+	private final AuthDomainServiceImpl authDomainService;
 
 	@Override
 	public AuthMember getValidatedMemberId(String accessToken) {
-		GoogleTokenValidationResponse response = googleOAuthClient.validateToken(accessToken);
+		GoogleTokenValidationResponse response = getGoogleTokenValidationResponse(accessToken);
 
 		SocialMember socialMember = SocialMemberRepositoryUtils.getSocialMember(
 			socialMemberRepository,
 			response.userId());
 
 		return AuthMember.of(socialMember.getId(), accessToken);
+	}
+
+	private GoogleTokenValidationResponse getGoogleTokenValidationResponse(String accessToken) {
+		try {
+			return googleOAuthClient.validateToken(accessToken);
+		} catch (GoogleOAuthException e) {
+			if (authDomainService.checkExistsToken(accessToken)) {
+				throw new GoogleOAuthException(GoogleOAuthErrorCode.REFRESH_TOKEN);
+			}
+			throw new GoogleOAuthException(GoogleOAuthErrorCode.INVALID_TOKEN);
+		}
 	}
 }
