7b37dee1d82ee3ca162e9898f0c9a9bb5cfa463d
sjhjack <sjhjack@naver.com>
2024-11-23 15:45:16 +0900

    Feat(#41): 생일 요청 API의 response 추가

---
+++ client/google-oauth-client/src/main/java/com/wypl/googleoauthclient/data/response/BirthdayResponse.java
@@ -0,0 +1,45 @@
+package com.wypl.googleoauthclient.data.response;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Builder
+public class BirthdayResponse {
+	private String resourceName;
+	private String etag;
+	private List<Birthday> birthdays;
+
+	@Getter
+	@Builder
+	public static class Birthday {
+		private Metadata metadata;
+		private Date date;
+
+		@Getter
+		@Builder
+		public static class Metadata {
+			private boolean primary;
+			private Source source;
+
+			@Getter
+			@Builder
+			public static class Source {
+				private String type;
+				private String id;
+			}
+		}
+
+		@Getter
+		@Setter
+		public static class Date {
+			private int year;
+			private int month;
+			private int day;
+		}
+	}
+}
+
