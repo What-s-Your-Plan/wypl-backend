50897c3d7e552ed39d5937b2543daf9f50710f92
KIMSEI1124 <workju1124@gmail.com>
2024-11-23 13:48:04 +0900

    Feat(#48): EmbeddedRedis 환경 설정 작

---
+++ domain/redis-embedded-domain/src/main/java/com/seikim/redisembeddeddomain/EmbeddedRedisConfig.java
@@ -0,0 +1,61 @@
+package com.seikim.redisembeddeddomain;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.seikim.redisembeddeddomain.redis.*;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import redis.embedded.RedisServer;
+
+@Slf4j
+@Profile({"default", "local", "test"})
+@Configuration
+public class EmbeddedRedisConfig {
+	private static final String OS_NAME = System.getProperty("os.name");
+
+	@Value("${spring.data.redis.port}")
+	private int redisPort;
+
+	private RedisServer redisServer;
+
+	@PostConstruct
+	private void start() throws IOException {
+		RedisAvailablePortFind findAvailablePortUtils = getRedisAvailablePortFind();
+
+		int port = findAvailablePortUtils.isRedisRunning(redisPort)
+			? findAvailablePortUtils.findAvailablePort(redisPort)
+			: redisPort;
+		log.info("Embedded Redis Running Port : [{}]", port);
+
+		redisServer = new RedisServer(port);
+		redisServer.start();
+	}
+
+	@PreDestroy
+	private void stop() throws IOException {
+		if (redisServer != null) {
+			redisServer.stop();
+		}
+	}
+
+	private RedisAvailablePortFind getRedisAvailablePortFind() {
+		if (OS.MAC.contains(OS_NAME)) {
+			return new RedisAvailablePortFindForMac();
+		} else if (OS.WINDOWS.contains(OS_NAME)) {
+			return new RedisAvailablePortFindForWindows();
+		} else if (OS.UBUNTU.contains(OS_NAME)) {
+			return new RedisAvailablePortFindForUbuntu();
+		} else if (OS.LINUX.contains(OS_NAME)) {
+			return new RedisAvailablePortFindForLinux();
+		} else if (OS.DEBIAN.contains(OS_NAME)) {
+			return new RedisAvailablePortFindForDebian();
+		}
+		throw new IllegalArgumentException("Unsupported OS : " + OS_NAME);
+	}
+}
---
+++ domain/redis-embedded-domain/src/main/java/com/seikim/redisembeddeddomain/redis/OS.java
@@ -0,0 +1,23 @@
+package com.seikim.redisembeddeddomain.redis;
+
+import lombok.Getter;
+
+@Getter
+public enum OS {
+	MAC("Mac"),
+	WINDOWS("Windows 10"),
+	LINUX("Linux"),
+	UBUNTU("Ubuntu"),
+	DEBIAN("Debian"),
+	;
+
+	private final String value;
+
+	OS(String value) {
+		this.value = value;
+	}
+
+	public boolean contains(String osName) {
+		return osName.contains(value);
+	}
+}
