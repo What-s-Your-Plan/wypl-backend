625b06e32baa634e54caec7aeafcaec59d0d1a65
sjhjack <sjhjack@naver.com>
2024-12-07 17:02:56 +0900

    Feat(#41): 토큰 재발행 테스트 코드 작성

--- application/wypl-core/src/test/java/com/wypl/wyplcore/auth/service/AuthServiceImplTest.java
+++ application/wypl-core/src/test/java/com/wypl/wyplcore/auth/service/AuthServiceImplTest.java
@@ -12,6 +12,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -20,6 +23,8 @@ import com.wypl.authdomain.auth.service.AuthDomainServiceImpl;
 import com.wypl.googleoauthclient.GoogleOAuthClient;
 import com.wypl.googleoauthclient.data.response.GoogleTokenResponse;
 import com.wypl.googleoauthclient.data.response.GoogleUserInfoResponse;
+import com.wypl.googleoauthclient.exception.GoogleOAuthErrorCode;
+import com.wypl.googleoauthclient.exception.GoogleOAuthException;
 import com.wypl.jpamemberdomain.member.OauthProvider;
 import com.wypl.jpamemberdomain.member.data.MemberDto;
 import com.wypl.jpamemberdomain.member.data.SocialMemberDto;
@@ -84,7 +89,6 @@ class AuthServiceImplTest {
 			given(socialMemberRepository.existsByOauthProviderAndOauthId(any(OauthProvider.class), anyString()))
 				.willReturn(true);
 
-
 			given(socialMemberRepository.findByOauthProviderAndOauthId(any(OauthProvider.class), anyString()))
 				.willReturn(Optional.of(mockSocialMember));
 
@@ -121,6 +125,54 @@ class AuthServiceImplTest {
 			assertThat(result.memberId()).isEqualTo(2L);
 			assertThat(result.accessToken()).isEqualTo(mockGoogleTokenResponse.accessToken());
 			assertThat(result.refreshToken()).isEqualTo(mockGoogleTokenResponse.refreshToken());
+		}
+	}
+
+	@DisplayName("토큰 재발행 로직을 테스트한다.")
+	@Nested
+	class reissueTokenTest {
+		@DisplayName("유효하지 않은 리프레시 토큰으로 인해 인증되지 않은 회원 오류를 던진다.")
+		@ParameterizedTest
+		@ValueSource(strings = {"", "invalid Refresh Token"})
+		@NullSource
+		void invalidRefreshTokenTest(String refreshToken) {
+			// Given
+			given(authDomainService.getRefreshToken(anyString()))
+				.willReturn(refreshToken);
+
+			// When & Then
+			assertThatThrownBy(() -> authService.reissueToken("accessToken", "refreshToken"))
+				.isInstanceOf(GoogleOAuthException.class)
+				.hasMessageContaining(GoogleOAuthErrorCode.NOT_AUTHORIZATION_MEMBER.getMessage());
+		}
+
+		@DisplayName("토큰을 정상적으로 재발행한다.")
+		@Test
+		void reissueSuccessTest() {
+			// Given
+			GoogleTokenResponse mockGoogleTokenResponse = new GoogleTokenResponse(
+				"accessToken",
+				3600,
+				"refreshToken",
+				"scope",
+				"idToken",
+				"tokenType"
+			);
+
+			given(authDomainService.getRefreshToken(anyString()))
+				.willReturn("refreshToken");
+
+			given(googleOAuthClient.fetchRefreshGoogleOAuthToken(anyString()))
+				.willReturn(mockGoogleTokenResponse);
+
+			// When
+			AuthTokensResponse result = authService.reissueToken("accessToken", "refreshToken");
+
+			// Then
+			verify(authDomainService).deleteToken(anyString());
+			verify(authDomainService).saveToken(anyString(), anyString());
+			assertThat(result.accessToken()).isEqualTo(mockGoogleTokenResponse.accessToken());
+			assertThat(result.refreshToken()).isEqualTo("refreshToken");
 		}
 	}
 }
