3f6ecf62337195d9b5d899c83b8e929a1cdf719b
sjhjack <sjhjack@naver.com>
2024-12-28 15:32:52 +0900

    Feat(#41): SocialMember 중복 저장 테스트 추가

--- domain/jpa-member-domain/src/test/java/com/wypl/jpamemberdomain/SocialMemberRepositoryTest.java
+++ domain/jpa-member-domain/src/test/java/com/wypl/jpamemberdomain/SocialMemberRepositoryTest.java
@@ -4,10 +4,13 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DuplicateKeyException;
 
 import com.wypl.jpamemberdomain.member.OauthProvider;
 import com.wypl.jpamemberdomain.member.data.MemberDto;
@@ -16,35 +19,64 @@ import com.wypl.jpamemberdomain.member.domain.Member;
 import com.wypl.jpamemberdomain.member.domain.SocialMember;
 import com.wypl.jpamemberdomain.member.repository.SocialMemberRepository;
 
-@SpringBootTest
+@DataJpaTest
 public class SocialMemberRepositoryTest {
 	@Autowired
 	private SocialMemberRepository socialMemberRepository;
 
 	@DisplayName("SocialMember를 저장한다.")
-	@Test
-	void saveSocialMemberTest() {
-		// Given
-		MemberDto memberDtoMock = MemberDto.builder()
-			.email("email")
-			.nickname("nickname")
-			.birthday(LocalDate.of(1999, 1, 15))
-			.profileImage("profileImage")
-			.build();
+	@Nested
+	class socialMemberSaveTest {
+		private SocialMemberDto socialMemberDtoMock;
+		private Member member;
+		private SocialMember socialMember;
 
-		SocialMemberDto socialMemberDtoMock = SocialMemberDto.builder()
-			.oauthProvider(OauthProvider.GOOGLE)
-			.oauthId("oauthId")
-			.build();
+		@BeforeEach
+		void beforeEach() {
+			MemberDto memberDtoMock = MemberDto.builder()
+				.email("email")
+				.nickname("nickname")
+				.birthday(LocalDate.of(1999, 1, 15))
+				.profileImage("profileImage")
+				.build();
 
-		// When
-		SocialMember result = socialMemberRepository.save(SocialMember.of(Member.of(memberDtoMock), socialMemberDtoMock));
-		SocialMember findSocialMember = socialMemberRepository.findById(result.getId()).get();
+			socialMemberDtoMock = SocialMemberDto.builder()
+				.oauthProvider(OauthProvider.GOOGLE)
+				.oauthId("oauthId")
+				.build();
 
-		// Then
-		assertThat(result.getId()).isEqualTo(findSocialMember.getId());
-		assertThat(result.getMember().getMemberId()).isEqualTo(findSocialMember.getMember().getMemberId());
-		assertThat(result.getOauthId()).isEqualTo(findSocialMember.getOauthId());
-		assertThat(result.getOauthProvider()).isEqualTo(findSocialMember.getOauthProvider());
+			member = Member.of(memberDtoMock);
+
+			socialMember = SocialMember.of(member, socialMemberDtoMock);
+		}
+
+		@DisplayName("SocialMember를 성공적으로 저장한다.")
+		@Test
+		void saveSuccessTest() {
+			// When
+			SocialMember result = socialMemberRepository.save(socialMember);
+			SocialMember findSocialMember = socialMemberRepository.findById(result.getId()).get();
+
+			// Then
+			assertThat(result.getId()).isEqualTo(findSocialMember.getId());
+			assertThat(result.getMember().getMemberId()).isEqualTo(findSocialMember.getMember().getMemberId());
+			assertThat(result.getOauthId()).isEqualTo(findSocialMember.getOauthId());
+			assertThat(result.getOauthProvider()).isEqualTo(findSocialMember.getOauthProvider());
+		}
+
+		@DisplayName("중복된 SocialMember 저장에 실패한다.")
+		@Test
+		void duplicatedMemberSaveTest() {
+			// Given
+			SocialMember duplicatedSocialMember = SocialMember.of(member, socialMemberDtoMock);
+
+			// When
+			socialMemberRepository.save(socialMember);
+
+			// Then
+			assertThatThrownBy(() -> socialMemberRepository.save(duplicatedSocialMember))
+				.isInstanceOf(DuplicateKeyException.class)
+				.hasMessageContaining("A different object with the same identifier value was already associated with the session");
+		}
 	}
 }
