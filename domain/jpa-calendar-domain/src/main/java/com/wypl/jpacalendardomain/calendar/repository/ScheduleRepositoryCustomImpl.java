5bd8b96cc3ebf84f3e034522a592bc5fd96888ae
jiwonhan <jiwons0803@naver.com>
2024-12-28 18:07:37 +0900

    refactor(#38): Q클래스 static import 적용
    Resolves: #38

--- domain/jpa-calendar-domain/src/main/java/com/wypl/jpacalendardomain/calendar/repository/ScheduleRepositoryCustomImpl.java
+++ domain/jpa-calendar-domain/src/main/java/com/wypl/jpacalendardomain/calendar/repository/ScheduleRepositoryCustomImpl.java
@@ -1,10 +1,11 @@
 package com.wypl.jpacalendardomain.calendar.repository;
 
+import static com.wypl.jpacalendardomain.calendar.domain.QSchedule.*;
+
 import java.time.LocalDate;
 import java.util.List;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.wypl.jpacalendardomain.calendar.domain.QSchedule;
 import com.wypl.jpacalendardomain.calendar.domain.Schedule;
 
 import lombok.RequiredArgsConstructor;
@@ -13,15 +14,13 @@ import lombok.RequiredArgsConstructor;
 public class ScheduleRepositoryCustomImpl implements ScheduleRepositoryCustom {
 
 	private final JPAQueryFactory jpaQueryFactory;
-	private final QSchedule schedule = QSchedule.schedule;
 
 	@Override
 	public List<Schedule> findByCalendarIdAndBetweenStartDateAndEndDate(long calendarId, LocalDate startDate,
 		LocalDate endDate) {
 		return jpaQueryFactory.selectFrom(schedule)
 			.where(schedule.scheduleInfo.calendar.id.eq(calendarId)
-				.and(schedule.repetitionStartDate.loe(endDate)
-					.and(schedule.repetitionEndDate.goe(startDate))))
+				.and(schedule.repetitionStartDate.loe(endDate).and(schedule.repetitionEndDate.goe(startDate))))
 			.fetch();
 	}
 }
