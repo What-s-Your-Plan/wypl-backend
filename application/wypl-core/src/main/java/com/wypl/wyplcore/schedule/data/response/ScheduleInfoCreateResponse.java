3b42c4929a8f64d59cc35c8c134c118b2f0b151a
KIMSEI1124 <workju1124@gmail.com>
2024-12-28 17:04:17 +0900

    Feat(#51): static method 생성

--- application/wypl-core/src/main/java/com/wypl/wyplcore/schedule/data/response/ScheduleInfoCreateResponse.java
+++ application/wypl-core/src/main/java/com/wypl/wyplcore/schedule/data/response/ScheduleInfoCreateResponse.java
@@ -1,14 +1,24 @@
 package com.wypl.wyplcore.schedule.data.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wypl.jpacalendardomain.calendar.domain.Schedule;
+import com.wypl.jpacalendardomain.calendar.domain.ScheduleInfo;
 
-public record ScheduleInfoCreateResponse (
+public record ScheduleInfoCreateResponse(
 
-        @JsonProperty("calendar_id")
-        Long calendarId,
+	@JsonProperty("calendar_id")
+	Long calendarId,
 
-        @JsonProperty("schedule_info_id")
-        Long scheduleInfoId
+	@JsonProperty("schedule_info_id")
+	Long scheduleInfoId
 
-){
+) {
+
+	public static ScheduleInfoCreateResponse from(Schedule schedule) {
+		ScheduleInfo scheduleInfo = schedule.getScheduleInfo();
+		return new ScheduleInfoCreateResponse(
+			scheduleInfo.getCalendar().getId(),
+			scheduleInfo.getId()
+		);
+	}
 }
