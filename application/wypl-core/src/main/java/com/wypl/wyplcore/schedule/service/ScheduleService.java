f297ffecac43b5997f68a7a0d24c9d54b9f4990b
KIMSEI1124 <workju1124@gmail.com>
2024-12-28 17:04:38 +0900

    Feat(#51): 일정 생성 기능 구현
    - 검증 기능 추가

--- application/wypl-core/src/main/java/com/wypl/wyplcore/schedule/service/ScheduleService.java
+++ application/wypl-core/src/main/java/com/wypl/wyplcore/schedule/service/ScheduleService.java
@@ -9,35 +9,42 @@ import com.wypl.jpacalendardomain.calendar.domain.Schedule;
 import com.wypl.jpacalendardomain.calendar.domain.ScheduleInfo;
 import com.wypl.jpacalendardomain.calendar.mapper.ScheduleInfoMapper;
 import com.wypl.jpacalendardomain.calendar.mapper.ScheduleMapper;
+import com.wypl.jpacalendardomain.calendar.repository.CalendarRepository;
 import com.wypl.jpacalendardomain.calendar.repository.ScheduleInfoRepository;
 import com.wypl.jpacalendardomain.calendar.repository.ScheduleRepository;
+import com.wypl.jpacalendardomain.calendar.utils.CalendarRepositoryUtils;
 import com.wypl.jpamemberdomain.member.domain.Member;
+import com.wypl.jpamemberdomain.member.repository.MemberRepository;
+import com.wypl.jpamemberdomain.member.utils.MemberRepositoryUtils;
 import com.wypl.wyplcore.schedule.data.request.ScheduleCreateRequest;
 import com.wypl.wyplcore.schedule.data.response.ScheduleInfoCreateResponse;
 
 import lombok.RequiredArgsConstructor;
 
-
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
+	private final ScheduleRepository scheduleRepository;
+	private final ScheduleInfoRepository scheduleInfoRepository;
 
-    private final ScheduleRepository scheduleRepository;
-    private final ScheduleInfoRepository scheduleInfoRepository;
+	private final MemberRepository memberRepository;
+	private final CalendarRepository calendarRepository;
 
-    @Transactional
-    public ScheduleInfoCreateResponse createSchedule(AuthMember authMember, ScheduleCreateRequest scheduleCreateRequest) {
+	@Transactional
+	public ScheduleInfoCreateResponse save(
+		final AuthMember authMember,
+		final ScheduleCreateRequest scheduleCreateRequest
+	) {
+		Calendar foundCalendar =
+			CalendarRepositoryUtils.findById(calendarRepository, scheduleCreateRequest.calenderId());
+		Member foundMember = MemberRepositoryUtils.findById(memberRepository, authMember.id());
 
-        Calendar foundCalendar = null;  // FIXME: scheduleInfoRequest의 calendarId로 찾는다. foundCalendar 엔티티 검증 필요.
-        Member foundMember = null; // FIXME: member 엔티티 검증 필요.
+		ScheduleInfo scheduleInfo = ScheduleInfoMapper.toJpaScheduleInfo(foundCalendar, foundMember.getMemberId());
+		ScheduleInfo savedScheduleInfo = scheduleInfoRepository.save(scheduleInfo);
 
-        ScheduleInfo scheduleInfo = ScheduleInfoMapper.toJpaScheduleInfo(foundCalendar, authMember.id());
-        Schedule schedule = ScheduleMapper.toJpaSchedule(scheduleCreateRequest, scheduleInfo);
+		Schedule schedule = ScheduleMapper.toJpaSchedule(scheduleCreateRequest, savedScheduleInfo);
+		Schedule savedSchedule = scheduleRepository.save(schedule);
 
-        ScheduleInfo savedScheduleInfo = scheduleInfoRepository.save(scheduleInfo);
-        Schedule savedSchedule = scheduleRepository.save(schedule);
-
-        return new ScheduleInfoCreateResponse(foundCalendar.getId(), savedScheduleInfo.getId());
-    }
-
+		return ScheduleInfoCreateResponse.from(savedSchedule);
+	}
 }
