5d7217ee9518b99b3aeaf70f25007d6d923ffeea
KIMSEI1124 <workju1124@gmail.com>
2024-12-30 07:59:24 +0900

    Feat(#51): Mockito 테스트 마이그레이션
    기존 DataJpaTest를 수정

--- domain/jpa-calendar-domain/src/test/java/com/wypl/jpacalendardomain/calendar/utils/CalendarRepositoryUtilsTest.java
+++ domain/jpa-calendar-domain/src/test/java/com/wypl/jpacalendardomain/calendar/utils/CalendarRepositoryUtilsTest.java
@@ -1,49 +1,65 @@
 package com.wypl.jpacalendardomain.calendar.utils;
 
-import static com.wypl.jpacalendardomain.calendar.CalendarFixture.*;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.wypl.jpacalendardomain.calendar.domain.Calendar;
+import com.wypl.jpacalendardomain.calendar.CalendarFixture;
 import com.wypl.jpacalendardomain.calendar.exception.CalendarErrorCode;
 import com.wypl.jpacalendardomain.calendar.exception.CalendarException;
 import com.wypl.jpacalendardomain.calendar.repository.CalendarRepository;
 
-@DataJpaTest
+@ExtendWith(MockitoExtension.class)
 class CalendarRepositoryUtilsTest {
-	@Autowired
-	private CalendarRepository repository;
+
+	private final CalendarRepository repository;
+
+	public CalendarRepositoryUtilsTest(@Mock CalendarRepository repository) {
+		this.repository = repository;
+	}
 
 	@DisplayName("Calendar 조회 Util")
 	@Nested
 	class FindById {
 
-		private Calendar calendar;
+		private long calendarId;
 
 		@BeforeEach
 		public void setUp() {
-			calendar = wyplProject.toCalendar();
-			repository.save(calendar);
+			calendarId = ThreadLocalRandom.current().nextLong();
 		}
 
 		@DisplayName("Calendar 조회에 성공한다.")
-		@Test
-		void success() {
+		@ParameterizedTest
+		@EnumSource(CalendarFixture.class)
+		void success(CalendarFixture fixture) {
+			/* Given */
+			given(repository.findById(anyLong()))
+				.willReturn(Optional.of(fixture.toCalendar()));
+
 			/* When & Then */
-			assertThatCode(() -> CalendarRepositoryUtils.findById(repository, calendar.getId()))
+			assertThatCode(() -> CalendarRepositoryUtils.findById(repository, calendarId))
 				.doesNotThrowAnyException();
 		}
 
 		@DisplayName("Calendar 조회시 존재하지 않으면 예외를 던진다.")
 		@Test
 		void noSuchCalender() {
-			assertThatThrownBy(() -> CalendarRepositoryUtils.findById(repository, calendar.getId() + 1_000L))
+			/* When & Then */
+			assertThatThrownBy(() -> CalendarRepositoryUtils.findById(repository, calendarId))
 				.isInstanceOf(CalendarException.class)
 				.hasMessageContaining(CalendarErrorCode.NO_SUCH_CALENDAR.getMessage());
 		}
