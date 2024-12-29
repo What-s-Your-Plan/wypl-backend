6472c854682eec908eb95c5d6e219a6fa8488c35
KIMSEI1124 <workju1124@gmail.com>
2024-12-29 20:44:56 +0900

    Feat(#51): Member 도메인 어노테이션 추가
    - Builder
    - NoArgsConstructor
    - AllArgsConstructor

---
+++ domain/jpa-member-domain/src/test/java/com/wypl/jpamemberdomain/member/fixture/MemberFixture.java
@@ -0,0 +1,48 @@
+package com.wypl.jpamemberdomain.member.fixture;
+
+import java.time.LocalDate;
+
+import com.wypl.common.Color;
+import com.wypl.jpamemberdomain.member.OauthProvider;
+import com.wypl.jpamemberdomain.member.TimeZone;
+import com.wypl.jpamemberdomain.member.domain.Member;
+
+public enum MemberFixture {
+	JEONG_HOON(1L, "biosjh@gmail.com", "sjh", LocalDate.of(1999, 1, 15), "JHprofile", OauthProvider.GOOGLE);
+
+	private final Long memberId;
+	private final String email;
+	private final String nickname;
+	private final LocalDate birthday;
+	private final String profileImage;
+	private final OauthProvider oauthProvider;
+
+	MemberFixture(
+		long memberId,
+		String email,
+		String nickname,
+		LocalDate birthday,
+		String profileImage,
+		OauthProvider oauthProvider
+	) {
+		this.memberId = memberId;
+		this.email = email;
+		this.nickname = nickname;
+		this.birthday = birthday;
+		this.profileImage = profileImage;
+		this.oauthProvider = oauthProvider;
+	}
+
+	public Member toMember() {
+		return Member.builder()
+			.memberId(memberId)
+			.email(email)
+			.nickname(nickname)
+			.birthday(birthday)
+			.profileImage(profileImage)
+			.color(Color.labelBrown)
+			.timeZone(TimeZone.KOREA)
+			.build();
+	}
+}
+
