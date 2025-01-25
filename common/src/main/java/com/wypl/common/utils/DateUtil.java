23f37941b0d77edc9382f8c622a3c93bf6faab52
jiwonhan <jiwons0803@naver.com>
2025-01-25 17:59:00 +0900

    refactor(#38): Year 반복 조회를 위한 findNextOrSame 수정
    as-is : dayOfYear 사용 -> Month, dayOfMonth 로 직접 구현
    
    Resolves: #38

--- common/src/main/java/com/wypl/common/utils/DateUtil.java
+++ common/src/main/java/com/wypl/common/utils/DateUtil.java
@@ -2,6 +2,7 @@ package com.wypl.common.utils;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.temporal.TemporalAdjusters;
 
 public class DateUtil {
@@ -30,16 +31,28 @@ public class DateUtil {
 	}
 
 	// Todo: 2/30 인데, 다음년도에는 2.29 까지밖에 없다면?
+
 	/**
-	 *  dayOfYear 를 기준으로, targetDate 와 같거나 그 이후에 해당하는 날짜를 찾는다.
-	 * @param targetDate 기준 날짜
-	 * @param dayOfYear from 1 to 366
-	 * @return LocalDate, not null
+	 * standardDate 이거나 그 이후의 month 와 dayOfMonth 를 만족하는 날짜를 찾는다.
+	 * @param standardDate : 기준이 되는 날짜
+	 * @param month : 탐색하려는 Month
+	 * @param dayOfMonth : 탐색하려는 dayOfMonth
+	 * @return 날짜
 	 */
-	public static LocalDate findNextOrSame(LocalDate targetDate, int dayOfYear) {
-		if (dayOfYear < targetDate.getDayOfYear()) {
-			return targetDate.plusYears(1).withDayOfYear(dayOfYear);
+	public static LocalDate findNextOrSame(LocalDate standardDate, Month month, int dayOfMonth) {
+		if (isAfter(standardDate.getMonth(), standardDate.getDayOfMonth(), month, dayOfMonth)) {
+			return LocalDate.of(standardDate.getYear() + 1, month, dayOfMonth);
 		}
-		return targetDate.withDayOfYear(dayOfYear);
+		return LocalDate.of(standardDate.getYear(), month, dayOfMonth);
+	}
+
+	private static boolean isAfter(Month firstMonth, int firstDayOfMonth, Month secondMonth, int secondDayOfMonth) {
+		if (firstMonth.getValue() > secondMonth.getValue()) {
+			return true;
+		}
+		if (firstMonth.getValue() == secondMonth.getValue()) {
+			return firstDayOfMonth > secondDayOfMonth;
+		}
+		return false;
 	}
 }
