98a55a3da8aa8d9dbbf6bb9dc991f94e2a2c240f
jiwonhan <jiwons0803@naver.com>
2025-01-25 16:52:20 +0900

    feat(#38): Add DateUtil.java
    Resolves: #38

--- common/src/main/java/com/wypl/common/utils/DateUtil.java
+++ common/src/main/java/com/wypl/common/utils/DateUtil.java
@@ -7,25 +7,39 @@ import java.time.temporal.TemporalAdjusters;
 public class DateUtil {
 
 	/**
-	 * 기준 날짜와 같거나 이후의 dayOfYear를 갖는 날짜를 찾는다.
-	 * @param dayOfYear from 1 to 366
+	 * 기준 날짜와 같거나 이후의 가장 가까운 dayOfWeek에 해당하는 날짜를 찾는다.
 	 * @param targetDate 기준 날짜
+	 * @param dayOfWeek 요일, from 1 to 7
 	 * @return LocalDate, not null
 	 */
-	public static LocalDate getNextOrSame(int dayOfYear, LocalDate targetDate) {
+	public static LocalDate findNextOrSameByDayOfWeek(LocalDate targetDate, DayOfWeek dayOfWeek) {
+		return targetDate.with(TemporalAdjusters.nextOrSame(dayOfWeek));
+	}
+
+	/**
+	 * 기준 날짜와 같거나 이후의 dayOfMonth를 갖는 날짜를 찾는다.
+	 * @param targetDate 기준 날짜
+	 * @param dayOfMonth from 1 to 31
+	 * @return LocalDate, not null
+	 */
+	public static LocalDate findNextOrSameByDayOfMonth(LocalDate targetDate, int dayOfMonth) {
+		if (dayOfMonth < targetDate.getDayOfMonth()) {
+			return targetDate.plusMonths(1).withDayOfMonth(dayOfMonth);
+		}
+		return targetDate.withDayOfMonth(dayOfMonth);
+	}
+
+	// Todo: 2/30 인데, 다음년도에는 2.29 까지밖에 없다면?
+	/**
+	 *  dayOfYear 를 기준으로, targetDate 와 같거나 그 이후에 해당하는 날짜를 찾는다.
+	 * @param targetDate 기준 날짜
+	 * @param dayOfYear from 1 to 366
+	 * @return LocalDate, not null
+	 */
+	public static LocalDate findNextOrSame(LocalDate targetDate, int dayOfYear) {
 		if (dayOfYear < targetDate.getDayOfYear()) {
 			return targetDate.plusYears(1).withDayOfYear(dayOfYear);
 		}
 		return targetDate.withDayOfYear(dayOfYear);
-	}
-
-	/**
-	 * 기준 날짜와 같거나 이후의 가장 가까운 dayOfWeek에 해당하는 날짜를 찾는다.
-	 * @param dayOfWeek 요일, from 1 to 7
-	 * @param targetDate 기준 날짜
-	 * @return LocalDate, not null
-	 */
-	public static LocalDate getNextOrSame(DayOfWeek dayOfWeek, LocalDate targetDate) {
-		return targetDate.with(TemporalAdjusters.nextOrSame(dayOfWeek));
 	}
 }
