d99b23eb9fe4b44a2d15fc8482e3bda2125efd48
sjhjack <sjhjack@naver.com>
2025-01-31 15:50:25 +0900

    Chore(#67): parameter에 final 키워드 추가

--- application/wypl-core/src/main/java/com/wypl/wyplcore/facade/AuthMemberFacadeImpl.java
+++ application/wypl-core/src/main/java/com/wypl/wyplcore/facade/AuthMemberFacadeImpl.java
@@ -61,7 +61,7 @@ public class AuthMemberFacadeImpl implements AuthMemberFacade {
 
 	@Override
 	@Transactional
-	public void quitMember(AuthMember authMember) {
+	public void quitMember(final AuthMember authMember) {
 		// Todo : 회원 탈퇴 로직 논의
 		// Todo : deleteToken은 스프링에서 비동기 처리하고(토큰이 아니라 멤버 탈퇴가 중요한거다), deleteMember는 Member 쪽으로 옮기자
 		deleteToken(authMember);
