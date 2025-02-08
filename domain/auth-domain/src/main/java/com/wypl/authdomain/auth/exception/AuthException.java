9ccd4d35908ba12058b61ccc620fe25057062b70
sjhjack <sjhjack@naver.com>
2025-02-08 16:46:08 +0900

    Remove(#71): 사용하지 않는 파일 제거

--- domain/auth-domain/src/main/java/com/wypl/authdomain/auth/exception/AuthException.java
+++
@@ -1,9 +0,0 @@
-// package com.wypl.authdomain.auth.exception;
-//
-// import com.wypl.common.exception.WyplException;
-//
-// public class AuthException extends WyplException {
-// 	public AuthException(AuthErrorCode serverErrorCode)	{
-// 		super(serverErrorCode);
-// 	}
-// }
