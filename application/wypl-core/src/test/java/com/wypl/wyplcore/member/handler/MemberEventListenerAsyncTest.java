07eed569a67b6798e913c7390a248d5f2a3d60c5
sjhjack <sjhjack@naver.com>
2025-08-10 17:14:15 +0900

    Chore(#71): 불필요한 테스트 코드 삭제

--- application/wypl-core/src/test/java/com/wypl/wyplcore/member/handler/MemberEventListenerAsyncTest.java
+++ application/wypl-core/src/test/java/com/wypl/wyplcore/member/handler/MemberEventListenerAsyncTest.java
@@ -2,62 +2,29 @@ package com.wypl.wyplcore.member.handler;
 
 import static org.mockito.Mockito.*;
 
-import java.util.concurrent.CountDownLatch;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.wypl.googleoauthclient.domain.AuthMember;
 import com.wypl.jpamemberdomain.member.repository.MemberRepository;
-import com.wypl.wyplcore.member.data.MemberEventDto;
 import com.wypl.wyplcore.member.service.MemberService;
 import com.wypl.wyplcore.token.service.TokenService;
 
 @SpringBootTest
 class MemberEventListenerAsyncTest {
-	// @Mock
-	@MockBean
-	private TokenService tokenService;
-	@Autowired
-	private ApplicationEventPublisher applicationEventPublisher;
-
 	@Autowired
 	private MemberService memberService;
 	@MockBean
+	private TokenService tokenService;
+	@MockBean
 	private MemberRepository memberRepository;
 
-	@DisplayName("Token을 비동기로 삭제한다.")
-	@Transactional
+	@DisplayName("회원 탈퇴 후 Refresh Token을 비동기로 삭제한다.")
 	@Test
 	void deleteToken_asynchronously() throws InterruptedException {
-		// Given
-		MemberEventDto memberEventDtoMock = new MemberEventDto("accessToken");
-		CountDownLatch countDownLatch = new CountDownLatch(1);
-
-		Mockito.doAnswer(invocation -> {
-			countDownLatch.await();
-			return null;
-		}).when(tokenService).deleteToken(Mockito.anyString());
-
-		// When
-		applicationEventPublisher.publishEvent(memberEventDtoMock);
-
-		// Then
-		Mockito.verify(tokenService, Mockito.never()).deleteToken(Mockito.anyString());
-		countDownLatch.countDown();
-		Thread.sleep(3000);
-		Mockito.verify(tokenService).deleteToken(Mockito.anyString());
-	}
-
-	@DisplayName("Token을 비동기로 삭제한다.")
-	@Test
-	void deleteToken_asynchronously2() throws InterruptedException {
 		// Given
 		AuthMember mockAuthMember = AuthMember.of(
 			1L,
@@ -72,5 +39,4 @@ class MemberEventListenerAsyncTest {
 		verify(memberRepository).deleteById(anyLong());
 		verify(tokenService).deleteToken(anyString());
 	}
-
 }
